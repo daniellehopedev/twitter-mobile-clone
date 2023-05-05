63ac23f93c52544e5a4c5b082c8df5c152d8a626
Danielle H <62916241+daniellehopedev@users.noreply.github.com>
2023-05-05 16:30:38 -0400

    created component for IconButton

--- components/Tweet.tsx
+++ components/Tweet.tsx
@@ -3,22 +3,10 @@ import { Entypo, EvilIcons } from '@expo/vector-icons';
 
 import { TweetType } from '../types';
 
+import IconButton  from './IconButton';
+
 type TweetProps = {
     tweet: TweetType
-}
-
-type IconButtonProps = {
-    icon: React.ComponentProps<typeof EvilIcons>['name'];
-    text?: number | string;
-}
-
-const IconButton = ({ icon, text }: IconButtonProps) => {
-    return (
-        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-            <EvilIcons name={icon} size={22} color="grey" />
-            <Text style={{ fontSize: 12, color: 'grey' }}>{text}</Text>
-        </View>
-    );
 }
 
 const Tweet = ({ tweet }: TweetProps) => {
