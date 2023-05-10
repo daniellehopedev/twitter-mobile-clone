6219351b1429ed36f5c6b19f7bf5b45325a7d717
Danielle H <62916241+daniellehopedev@users.noreply.github.com>
2023-05-09 22:20:10 -0400

    added 'add new tweet' button and screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,6 @@
 import { View, StyleSheet, FlatList } from 'react-native';
+import { Entypo } from '@expo/vector-icons';
+import { Link } from 'expo-router';
 
 import Tweet from '../../components/Tweet';
 import tweets from '../../assets/data/tweets';
@@ -7,6 +9,9 @@ export default function TabOneScreen() {
   return (
     <View style={styles.page}>
       <FlatList data={tweets} renderItem={({item}) => <Tweet tweet={item} /> }  />
+      <Link href="/new-tweet" asChild >
+        <Entypo name="plus" size={24} color="white" style={styles.floatingButton} />
+      </Link>
     </View>
   );
 }
@@ -15,5 +20,22 @@ const styles = StyleSheet.create({
   page: {
     backgroundColor: 'white',
     flex: 1
+  },
+  floatingButton: {
+    backgroundColor: '#1DA1F2',
+    borderRadius: 25,
+    padding: 15,
+    position: 'absolute',
+    right: 15,
+    bottom: 15,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+    overflow: 'hidden'
   }
 });
---
+++ app/new-tweet.tsx
@@ -0,0 +1,13 @@
+import { View, StyleSheet, Text } from 'react-native';
+
+export default function NewTweet() {
+    return (
+        <View>
+            <Text>Create New Tweet</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+});
